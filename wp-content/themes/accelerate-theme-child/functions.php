62014d560c7cac0d060fbdf01245dcbbe82d05c0
gaelporteu <gael.porteu@gmail.com>
2020-10-30 13:46:57 -0400

    Add case study files

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -16,3 +16,22 @@ function accelerate_child_scripts(){
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
 }
 add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
+
+// Custom Post Types Function
+function create_custom_post_types() {
+// Add Case Studies post type
+    register_post_type( 'case_studies',
+        array(
+            'labels' => array(
+                'name' => __( 'Case Studies' ),
+                'singular_name' => __( 'Case Study' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array( 'slug' => 'case-studies' ),
+        )
+    );
+}
+
+// Hook this custom post type into the theme
+add_action( 'init', 'create_custom_post_types' );
